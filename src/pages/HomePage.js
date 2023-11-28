e704dee3bc78c07de0056959ab77cd5cbc2ba165
samet7ozturk <sametozturk973@gmail.com>
2023-11-28 23:45:05 +0300

    Create home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
+import Header from "../layout/Header";
+
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
     <div>
-      <button data-cy="orderpage-button">
-        <Link to="/login" className="order-button1">
-          <span>FRIENDS</span>
-        </Link>
-      </button>
+      <div>
+        <Header />
+      </div>
+      <main>
+        <button>
+          <Link to="/login" className="button1">
+            <span>FRIENDS</span>
+          </Link>
+        </button>
+      </main>
     </div>
   );
 };
