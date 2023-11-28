cc4258ebaeee56379c62f050d1c94aa48e9549bc
samet7ozturk <sametozturk973@gmail.com>
2023-11-28 21:32:29 +0300

    FriendsList page template

--- src/pages/FriendsListPage.js
+++ src/pages/FriendsListPage.js
@@ -1,9 +1,38 @@
-const LoginPage = () => {
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const FriendsListPage = () => {
+  const token = localStorage.getItem("token");
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/api/friends", {
+        headers: {
+          Authorization: `${token}`,
+        },
+      })
+      .then((res) => {
+        console.log("list", res.data);
+        setList(res.data);
+      })
+      .catch((error) => {
+        console.error("FriendsList error:", error);
+      });
+  }, [token]);
+
   return (
     <div>
       <h1>FRIEND LIST</h1>
+      <div>
+        {list.map((friend) => (
+          <div key={friend.id}>
+            {friend.name}-{friend.email}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default LoginPage;
+export default FriendsListPage;
