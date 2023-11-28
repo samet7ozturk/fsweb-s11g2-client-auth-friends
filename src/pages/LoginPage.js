e6a2c3c17d723e2c625839901c47a718113a66d1
samet7ozturk <sametozturk973@gmail.com>
2023-11-28 23:50:33 +0300

    Login page regulation

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,6 +2,7 @@ import { useHistory } from "react-router-dom";
 // import { AxiosInstance } from "../api/api";
 import axios from "axios";
 import { useForm } from "react-hook-form";
+import Header from "../layout/Header";
 
 const LoginPage = () => {
   //   const location = useLocation();
@@ -21,23 +22,25 @@ const LoginPage = () => {
 
   return (
     <div>
-      <h1>LOGIN</h1>
-      <hr />
-      <form onSubmit={handleSubmit(submitHandler)}>
-        <div>
-          <label>
-            USERNAME
-            <input type="text" {...register("username")} />
-          </label>
-        </div>
-        <div>
-          <label>
-            PASSWORD
-            <input type="password" {...register("password")} />
-          </label>
-        </div>
-        <button type="submit">SUBMIT</button>
-      </form>
+      <Header />
+      <div className="loginpage">
+        <h1>LOGIN</h1>
+        <form onSubmit={handleSubmit(submitHandler)}>
+          <div>
+            <label>
+              USERNAME
+              <input type="text" {...register("username")} />
+            </label>
+          </div>
+          <div>
+            <label>
+              PASSWORD
+              <input type="password" {...register("password")} />
+            </label>
+          </div>
+          <button type="submit">SUBMIT</button>
+        </form>
+      </div>
     </div>
   );
 };
